112977ee72f823cac6631c0b3e78297f004d64d7
Julia Schäffer <juliaschaffer@MacBook-Pro.fritz.box>
2024-02-27 19:03:50 +0100

    add first functions

--- script.js
+++ script.js
@@ -3,9 +3,24 @@ async function takePokemon() {
     let response = await fetch(url);
     let responseAsJson = await response.json();
     let currentPokemon = responseAsJson['results'];
-    for (let p = 0; p< currentPokemon.length; p++){
-        
+    for (let p = 0; p < currentPokemon.length; p++) {
+        getPokemon(currentPokemon, p);
     }
-    
+}
+
+async function getPokemon(currentPokemon, p) {
+    let url = currentPokemon[p]['url'];
+    let response = await fetch(url);
+    let pokemon = await response.json();
+    cardTemplate(pokemon, p)
+}
+
+function cardTemplate(pokemon, p) {
+    console.log(pokemon);
+    let image = pokemon['sprites']['front_shiny'];
+    let name = pokemon['name'];
+    let baseExperience = pokemon['base_experience'];
+    return document.getElementById('card').innerHTML += `<div class="pokedexCard"> <div class="divLeft"> XP&ensp;<b>${baseExperience}</b> </div> <h2>${name}</h2> <img src="${image}"> <div class="cardinfo">  </div> </div>
+    `;
 }
 
